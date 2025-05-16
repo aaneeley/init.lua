af8adf9691d6197df8902d237d8c18bc5594886f
Andrew Neeley <andrew.neeley@icloud.com>
2025-05-16 14:24:37 -0400

    dev

--- lua/aaneeley/lazy/git.lua
+++ lua/aaneeley/lazy/git.lua
@@ -16,7 +16,7 @@ return {
 		-- setting the keybinding for LazyGit with 'keys' is recommended in
 		-- order to load the plugin when the command is run for the first time
 		keys = {
-			{ "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{ "<leader>g", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit" },
 		},
 	},
 	{ -- gitsigns
@@ -29,23 +29,6 @@ return {
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
-		},
-	},
-	{ -- blame
-		"f-person/git-blame.nvim",
-		-- load the plugin at startup
-		event = "VeryLazy",
-		-- Because of the keys part, you will be lazy loading this plugin.
-		-- The plugin will only load once one of the keys is used.
-		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
-		-- or lazy = false. One of both options will work.
-		opts = {
-			-- your configuration comes here
-			-- for example
-			enabled = true, -- if you want to enable the plugin
-			message_template = "Summary: *<summary>*\nTimestamp: *<date>*\nAuthor: *<author>*\nSha: <<sha>>", -- template for the blame message, check the Message template section for more options
-			date_format = "%r", -- template for the date, check Date format section for more options
-			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 		},
 	},
 }
